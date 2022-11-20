50f38b6fbb0ee689c749072d5b23237b40a6a18c
Nadilson <nadilson@protonmail.com>
2022-11-20 03:36:16 -0300

    Cards com dados estáticos, data em pt-BR

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
@@ -7,10 +7,21 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { registerLocaleData } from '@angular/common';
+import ptBr from '@angular/common/locales/pt';
+
+registerLocaleData(ptBr);
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'pt-BR',
+      useClass: IonicRouteStrategy,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  todoList = [
+    {
+      itemName: 'Programar',
+      itemDueDate: '11/25/2022',
+      itemPriority: 'High',
+      itemCategory: 'Work',
+    },
+    {
+      itemName: 'Desenhar app',
+      itemDueDate: '11/28/2022',
+      itemPriority: 'Low',
+      itemCategory: 'Work',
+    },
+    {
+      itemName: 'Comprar hardware',
+      itemDueDate: '11/30/2022',
+      itemPriority: 'Middle',
+      itemCategory: 'Personal',
+    },
+    {
+      itemName: 'Treinar',
+      itemDueDate: '12/01/2022',
+      itemPriority: 'High',
+      itemCategory: 'Personal',
+    },
+  ];
+
+  today: number = Date.now();
 
   constructor() {}
-
 }
